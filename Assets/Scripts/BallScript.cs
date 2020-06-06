d7c3d25a497a9e9e967ef8694585196372485846
shashanksingh <shashank.personal@gmail.com>
2020-06-06 14:23:19 +0100

    initial

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class BallScript : MonoBehaviour {
+    [SerializeField]
+    float forceValue = 4.5f;
+ 
+    Rigidbody2D myBody;
+    // Use this for initialization
+    void Start () {
+        myBody = GetComponent<Rigidbody2D>();
+        myBody.AddForce (new Vector2 (forceValue * 50, 50));
+    }
+ 
+    // Update is called once per frame
+    void Update () {
+ 
+    }
+}
---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class PaddleScript : MonoBehaviour {
+ 
+   [SerializeField]
+   bool isPlayerTwo;
+   [SerializeField]
+   float speed = 0.2f;       // how far the paddle moves per frame
+   Transform myTransform;    // reference to the object's transform
+ 
+   // Use this for initialization
+   void Start () {
+       myTransform = transform;
+   }
+     
+   // FixedUpdate is called once per physics tick/frame
+   void FixedUpdate () {
+       // first decide if this is player 1 or player 2 so we know what keys to listen for
+       if (isPlayerTwo)
+       {
+           if (Input.GetKey ("o"))
+               MoveUp ();
+           else if (Input.GetKey ("l"))
+               MoveDown ();
+       }
+       else // if not player 2 it must be player 1
+       {
+           if (Input.GetKey ("q"))
+               MoveUp ();
+           else if (Input.GetKey ("a"))
+               MoveDown ();
+       }
+    }
+ 
+   // move the player's paddle up by an amount determined by 'speed'
+   void MoveUp()
+   {
+       myTransform.position = new Vector2(myTransform.position.x, myTransform.position.y + speed);
+   }
+ 
+   // move the player's paddle down by an amount determined by 'speed'
+   void MoveDown()
+   {
+       myTransform.position = new Vector2 (myTransform.position.x, myTransform.position.y - speed);            
+   }
+}
